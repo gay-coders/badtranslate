3e912d5873423a2afd4c7b62a84ceb77f4d9f1fc
northlightz <lepyrogang@proton.me>
2025-06-05 18:29:29 +0330

    feat: the application is now usable via cli-arguments

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,82 @@
 mod translate;
-use std::{collections::HashMap, fs::File, io};
-
 use crate::translate::{bt_random_run, bt_run};
+use clap::{Parser, Subcommand};
+use std::{
+    collections::{BTreeMap, HashMap},
+    fs::File,
+    io::{self, Write},
+};
 use translate::bt_normal_translate;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct CLI {
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Translate normally from one language to another")]
+    Translate {
+        input_string: String,
+        from_lang: String,
+        to_lang: String,
+    },
+    #[command(
+        about = "Translate to each language (within order of the json file) and then back to english to get the mess"
+    )]
+    GibberInOrder {
+        input_string: String,
+        iteration: usize,
+    },
+    #[command(about = "Translate to each language and then back to english to get the mess")]
+    GibberRandom {
+        input_string: String,
+        iteration: usize,
+    },
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let raw_json: File = File::open("src/lang-list.jsonc").expect("Couldn't open json file");
-    let json_reader: io::BufReader<File> = io::BufReader::new(raw_json);
-    let languages: HashMap<String, String> = serde_json::from_reader(json_reader)?;
+    let cli: CLI = CLI::parse();
 
-    let sample_text: &str = "real";
-
-    let mut response: String;
-
-    response = bt_normal_translate(sample_text, Some("en"), Some("zh-CN")).await?;
-    println!("[Translate FROM English TO Simplified-Chinese]:\n{response}");
-
-    response = bt_run(sample_text, &languages, Some(10)).await?;
-    println!("[Translate through the FIRST 10 langs]:\n{response}");
-
-    response = bt_random_run(sample_text, &languages, Some(10)).await?;
-    println!("[badtranslate_random with 10 iterations returned]:\n{response}");
-
-    response = bt_run(sample_text, &languages, None).await?;
-    println!("[Translate through the ALL langs in order]:\n{response}");
-
-    response = bt_random_run(sample_text, &languages, None).await?;
-    println!("[Translate through the ALL langs in random order]:\n{response}");
-
+    match &cli.command {
+        Commands::Translate {
+            input_string,
+            from_lang,
+            to_lang,
+        } => {
+            let response =
+                bt_normal_translate(input_string.as_str(), Some(from_lang), Some(to_lang)).await?;
+            println!("{input_string} FROM {from_lang} to {to_lang}:\n{response}");
+            println!("-------------------------------------------");
+        }
+        Commands::GibberInOrder {
+            input_string,
+            iteration,
+        } => {
+            let raw_json: File =
+                File::open("src/lang-list.jsonc").expect("Couldn't open json file");
+            let json_reader: io::BufReader<File> = io::BufReader::new(raw_json);
+            let languages: BTreeMap<String, String> = serde_json::from_reader(json_reader)?;
+            let response = bt_run(input_string.as_str(), &languages, Some(*iteration)).await?;
+            println!("Back to English:\n{response}");
+            println!("-------------------------------------------");
+        }
+        Commands::GibberRandom {
+            input_string,
+            iteration,
+        } => {
+            let raw_json: File =
+                File::open("src/lang-list.jsonc").expect("Couldn't open json file");
+            let json_reader: io::BufReader<File> = io::BufReader::new(raw_json);
+            let languages: HashMap<String, String> = serde_json::from_reader(json_reader)?;
+            let response =
+                bt_random_run(input_string.as_str(), &languages, Some(*iteration)).await?;
+            println!("Back to English:\n{response}");
+            println!("-------------------------------------------");
+        }
+    }
     Ok(())
 }
