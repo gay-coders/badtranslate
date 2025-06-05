7afd1aad4a778c57a6a0b536e953c40570da30d2
meqativ <meqativ@users.noreply.github.com>
2025-06-05 04:20:00 +0300

    tidy up stuff and add a todo method

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 mod translate;
 use std::{collections::HashMap, fs::File, io};
-use translate::bt_translate;
+use translate::translate;
 
-use crate::translate::bt_badtranslate;
+use crate::translate::{badtranslate, badtranslate_random};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let json_lang_list = File::open("src/lang-list.json").expect("Couldn't open json file");
-    let json_reader = io::BufReader::new(json_lang_list);
-    let json_data: HashMap<String, String> = serde_json::from_reader(json_reader)?;
+    let raw_json = File::open("src/lang-list.json").expect("Couldn't open json file");
+    let json_reader = io::BufReader::new(raw_json);
+    let languages: HashMap<String, String> = serde_json::from_reader(json_reader)?;
 
-    let sample_text: &str = "meow this is me, maow maow\n meoas is this being translated at all";
+    let sample_text: &str = "meow this is me, maow maow\n miaouw is this being translated at all";
 
     let mut response: String;
 
     // original translate function
-    response = bt_translate(sample_text, Some("en"), Some("uk")).await?;
-    println!("bt_translate(en to uk) returned: {response}\n");
+    response = translate(sample_text, Some("en"), Some("uk")).await?;
+    println!("translate(en to uk) returned: {response}\n");
     response.clear();
 
     // translate to every language and back to english
-    response = bt_badtranslate(sample_text, json_data).await?;
-    println!("bt_badtranslate returned: {response}");
+    response = badtranslate(sample_text, &languages).await?;
+    println!("badtranslate returned: {response}");
 
+		println!("badtranslate_random with 10 iterations returned: {}", badtranslate_random(sample_text, &languages).await?);
     Ok(())
 }
--- src/translate.rs
+++ src/translate.rs
@@ -3,63 +3,52 @@ use regex::Regex;
 use serde_json::Value;
 use std::collections::HashMap;
 
-pub async fn bt_translate(
+pub async fn translate(
     input: &str,
     from: Option<&str>,
     to: Option<&str>,
 ) -> Result<String, Box<dyn std::error::Error>> {
-    let from_language = from.unwrap_or("auto");
-    let to_language = to.unwrap_or("en");
-    let input = strip_discord_emojis(input);
-    let body: Value = reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl={from_language}&tl={to_language}&dt=t&q={}", utf8_percent_encode(input.as_str(), NON_ALPHANUMERIC))).await?.json().await?;
-
-    if let Some(array) = body.get(0).and_then(|v| v.as_array()) {
-        let result = array
-            .iter()
-            .filter_map(|s| s.get(0))
-            .filter_map(|v| v.as_str())
-            .collect::<String>();
-
-        Ok(result)
-    } else {
-        Ok(String::new())
-    }
+	let from_language = from.unwrap_or("auto");
+	let to_language = to.unwrap_or("en");
+	let input = strip_discord_emojis(input);
+	let body: Value = reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl={from_language}&tl={to_language}&dt=t&q={}", utf8_percent_encode(input.as_str(), NON_ALPHANUMERIC))).await?.json().await?;
+	gtranslate_json_to_string(body)
 }
 
-// you could remove the temp_abom(ination) string variable and just use the input and make it mutable
-// this is your preference i didn't touch it
-
-pub async fn bt_badtranslate(
+pub async fn badtranslate(
     input: &str,
-    json_data: HashMap<String, String>,
+    languages: &HashMap<String, String>,
 ) -> Result<String, Box<dyn std::error::Error>> {
-    let mut temp_abom: String = String::from(input);
-    let mut google_input: String;
+    let mut current_translation: String = String::from(input);
 
     // translate through all langs in the json data (google translate has 153 here e.g.)
-    for (code, lang) in json_data {
-        google_input = bt_beautify_response(reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl={code}&dt=t&q={}", utf8_percent_encode(temp_abom.as_str(), NON_ALPHANUMERIC))).await?.json().await?).unwrap();
-        temp_abom = google_input.clone();
-        println!("At language {lang}: {temp_abom}");
+    for (code, lang) in languages {
+        current_translation = translate(&current_translation, Some("auto"), Some(code.as_str()))
+					.await.unwrap();
+        println!("At language {lang}: {current_translation}");
     }
+    current_translation = translate(&current_translation.to_string(),Some("auto"),Some("en"))
+			.await.unwrap(); //bt_beautify_response(reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q={}", utf8_percent_encode(input, NON_ALPHANUMERIC))).await?.json().await?).unwrap();
+    println!("Back to english: {current_translation}");
 
-    google_input = bt_beautify_response(reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q={}", utf8_percent_encode(input, NON_ALPHANUMERIC))).await?.json().await?).unwrap();
-    println!("Back to english: {google_input}");
-
-    Ok(google_input)
+    Ok(current_translation)
 }
 
-fn bt_beautify_response(google_input: Value) -> Result<String, Box<dyn std::error::Error>> {
+pub async fn badtranslate_random(
+	input: &str,
+	languages: &HashMap<String, String>,
+) -> Result<String, Box<dyn std::error::Error>> {
+	todo!();
+}
+fn gtranslate_json_to_string(google_input: Value) -> Result<String, Box<dyn std::error::Error>> {
     if let Some(array) = google_input.get(0).and_then(|v| v.as_array()) {
-        let result = array
+        Ok(array
             .iter()
             .filter_map(|s| s.get(0))
             .filter_map(|v| v.as_str())
             .collect::<String>()
             .trim()
-            .to_string();
-
-        Ok(result)
+            .to_string())
     } else {
         Ok(String::new())
     }
