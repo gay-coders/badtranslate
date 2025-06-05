0e82ad69c3d01fb9c80593f645b278c1c809c675
northlightz <lepyrogang@proton.me>
2025-06-05 13:01:15 +0330

    A little tidy up for me locally

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod translate;
 use std::{collections::HashMap, fs::File, io};
-use translate::translate;
 
+use translate::translate;
 use crate::translate::{badtranslate, badtranslate_random};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let raw_json = File::open("src/lang-list.json").expect("Couldn't open json file");
-    let json_reader = io::BufReader::new(raw_json);
+    let raw_json: File = File::open("src/lang-list.json").expect("Couldn't open json file");
+    let json_reader: io::BufReader<File> = io::BufReader::new(raw_json);
     let languages: HashMap<String, String> = serde_json::from_reader(json_reader)?;
 
     let sample_text: &str = "meow this is me, maow maow\n miaouw is this being translated at all";
@@ -23,6 +23,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     response = badtranslate(sample_text, &languages).await?;
     println!("badtranslate returned: {response}");
 
-		println!("badtranslate_random with 10 iterations returned: {}", badtranslate_random(sample_text, &languages).await?);
+    println!("badtranslate_random with 10 iterations returned: {}", badtranslate_random(sample_text, &languages).await?);
     Ok(())
 }
--- src/translate.rs
+++ src/translate.rs
@@ -8,11 +8,11 @@ pub async fn translate(
     from: Option<&str>,
     to: Option<&str>,
 ) -> Result<String, Box<dyn std::error::Error>> {
-	let from_language = from.unwrap_or("auto");
-	let to_language = to.unwrap_or("en");
-	let input = strip_discord_emojis(input);
-	let body: Value = reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl={from_language}&tl={to_language}&dt=t&q={}", utf8_percent_encode(input.as_str(), NON_ALPHANUMERIC))).await?.json().await?;
-	gtranslate_json_to_string(body)
+    let from_language: &str = from.unwrap_or("auto");
+    let to_language: &str = to.unwrap_or("en");
+    let input: String = strip_discord_emojis(input);
+    let body: Value = reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl={from_language}&tl={to_language}&dt=t&q={}", utf8_percent_encode(input.as_str(), NON_ALPHANUMERIC))).await?.json().await?;
+    gtranslate_json_to_string(body)
 }
 
 pub async fn badtranslate(
@@ -24,24 +24,26 @@ pub async fn badtranslate(
     // translate through all langs in the json data (google translate has 153 here e.g.)
     for (code, lang) in languages {
         current_translation = translate(&current_translation, Some("auto"), Some(code.as_str()))
-					.await.unwrap();
+            .await
+            .unwrap();
         println!("At language {lang}: {current_translation}");
     }
-    current_translation = translate(&current_translation.to_string(),Some("auto"),Some("en"))
-			.await.unwrap(); //bt_beautify_response(reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q={}", utf8_percent_encode(input, NON_ALPHANUMERIC))).await?.json().await?).unwrap();
+    current_translation = translate(&current_translation.to_string(), Some("auto"), Some("en"))
+        .await
+        .unwrap(); //bt_beautify_response(reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q={}", utf8_percent_encode(input, NON_ALPHANUMERIC))).await?.json().await?).unwrap();
     println!("Back to english: {current_translation}");
 
     Ok(current_translation)
 }
 
 pub async fn badtranslate_random(
-	input: &str,
-	languages: &HashMap<String, String>,
+    input: &str,
+    languages: &HashMap<String, String>,
 ) -> Result<String, Box<dyn std::error::Error>> {
-	todo!();
+    unimplemented!();
 }
 fn gtranslate_json_to_string(google_input: Value) -> Result<String, Box<dyn std::error::Error>> {
-    if let Some(array) = google_input.get(0).and_then(|v| v.as_array()) {
+    if let Some(array) = google_input.get(0).and_then(|v: &Value| v.as_array()) {
         Ok(array
             .iter()
             .filter_map(|s| s.get(0))
@@ -55,6 +57,6 @@ fn gtranslate_json_to_string(google_input: Value) -> Result<String, Box<dyn std:
 }
 
 fn strip_discord_emojis(input: &str) -> String {
-    let re = Regex::new(r"<a?:([a-zA-Z0-9_]+):\d+>").unwrap();
+    let re: Regex = Regex::new(r"<a?:([a-zA-Z0-9_]+):\d+>").unwrap();
     re.replace_all(input, "$1 ").to_string()
 }
