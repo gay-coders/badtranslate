279617cd84af5e50028f6e2d66a7443c7fb4d90f
northlightz <lepyrogang@proton.me>
2025-06-05 03:55:15 +0330

    Added bad translation mode, it will translate through every single language available in google translate api using the json file as its request parameters.
    have fun :3
    // northlightz

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 mod translate;
-use translate::translate_text;
+use std::{collections::HashMap, fs::File, io};
+use translate::bt_translate;
+
+use crate::translate::bt_badtranslate;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let response = translate_text("meow this is me, maow maow\n meoas is this being translated at all", Some("auto"), Some("uk")).await?;
-	println!("Translated text: {}", response);
-	Ok(())
+    let json_lang_list = File::open("src/lang-list.json").expect("Couldn't open json file");
+    let json_reader = io::BufReader::new(json_lang_list);
+    let json_data: HashMap<String, String> = serde_json::from_reader(json_reader)?;
+
+    let sample_text: &str = "meow this is me, maow maow\n meoas is this being translated at all";
+
+    let mut response: String;
+
+    // original translate function
+    response = bt_translate(sample_text, Some("en"), Some("uk")).await?;
+    println!("bt_translate(en to uk) returned: {response}\n");
+    response.clear();
+
+    // translate to every language and back to english
+    response = bt_badtranslate(sample_text, json_data).await?;
+    println!("bt_badtranslate returned: {response}");
+
+    Ok(())
 }
--- src/translate.rs
+++ src/translate.rs
@@ -1,29 +1,71 @@
-
-use serde_json::Value;
-use regex::Regex;
 use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
+use regex::Regex;
+use serde_json::Value;
+use std::collections::HashMap;
 
-pub async fn translate_text(input: &str, from: Option<&str>, to: Option<&str>) -> Result<String, Box<dyn std::error::Error>>{
-	let from_language = from.unwrap_or("auto");
-	let to_language = to.unwrap_or("en");
-	let input = strip_discord_emojis(input);
-	let body: Value = reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl={from_language}&tl={to_language}&dt=t&q={}", utf8_percent_encode(input.as_str(), NON_ALPHANUMERIC))).await?.json().await?;
+pub async fn bt_translate(
+    input: &str,
+    from: Option<&str>,
+    to: Option<&str>,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let from_language = from.unwrap_or("auto");
+    let to_language = to.unwrap_or("en");
+    let input = strip_discord_emojis(input);
+    let body: Value = reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl={from_language}&tl={to_language}&dt=t&q={}", utf8_percent_encode(input.as_str(), NON_ALPHANUMERIC))).await?.json().await?;
 
-	if let Some(array) = body.get(0).and_then(|v| v.as_array()) {
-		let result = array
-			.iter()
-			.filter_map(|s| s.get(0))
-			.filter_map(|v| v.as_str())
-			.collect::<String>();
+    if let Some(array) = body.get(0).and_then(|v| v.as_array()) {
+        let result = array
+            .iter()
+            .filter_map(|s| s.get(0))
+            .filter_map(|v| v.as_str())
+            .collect::<String>();
 
-		Ok(result)
-	} else {
-		Ok(String::new())
-	}
+        Ok(result)
+    } else {
+        Ok(String::new())
+    }
 }
 
+// you could remove the temp_abom(ination) string variable and just use the input and make it mutable
+// this is your preference i didn't touch it
+
+pub async fn bt_badtranslate(
+    input: &str,
+    json_data: HashMap<String, String>,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let mut temp_abom: String = String::from(input);
+    let mut google_input: String;
+
+    // translate through all langs in the json data (google translate has 153 here e.g.)
+    for (code, lang) in json_data {
+        google_input = bt_beautify_response(reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl={code}&dt=t&q={}", utf8_percent_encode(temp_abom.as_str(), NON_ALPHANUMERIC))).await?.json().await?).unwrap();
+        temp_abom = google_input.clone();
+        println!("At language {lang}: {temp_abom}");
+    }
+
+    google_input = bt_beautify_response(reqwest::get(format!("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q={}", utf8_percent_encode(input, NON_ALPHANUMERIC))).await?.json().await?).unwrap();
+    println!("Back to english: {google_input}");
+
+    Ok(google_input)
+}
+
+fn bt_beautify_response(google_input: Value) -> Result<String, Box<dyn std::error::Error>> {
+    if let Some(array) = google_input.get(0).and_then(|v| v.as_array()) {
+        let result = array
+            .iter()
+            .filter_map(|s| s.get(0))
+            .filter_map(|v| v.as_str())
+            .collect::<String>()
+            .trim()
+            .to_string();
+
+        Ok(result)
+    } else {
+        Ok(String::new())
+    }
+}
 
 fn strip_discord_emojis(input: &str) -> String {
-	let re = Regex::new(r"<a?:([a-zA-Z0-9_]+):\d+>").unwrap();
-	re.replace_all(input, "$1 ").to_string()
+    let re = Regex::new(r"<a?:([a-zA-Z0-9_]+):\d+>").unwrap();
+    re.replace_all(input, "$1 ").to_string()
 }
